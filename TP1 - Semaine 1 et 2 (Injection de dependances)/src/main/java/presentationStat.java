57a3fd9a739047d42fd32c0c5ea175dea7a64bfc
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-02-21 19:36:07 +0100

    TP1 - Injection de dependances Statique/Dynamique/Framework

--- TP1 - Semaine 1 et 2 (Injection de dependances)/src/main/java/presentationStat.java
+++ TP1 - Semaine 1 et 2 (Injection de dependances)/src/main/java/presentationStat.java
@@ -5,11 +5,14 @@ import metier.MetierImpl;
 public class presentationStat {
     public static void main(String[] args) {
         MetierImpl metier=new MetierImpl();
+        metier.setDao(new DaoImpl());
         System.out.println(metier.calcul());
 
         System.out.println("**********************");
 
         MetierImpl metierCapt=new MetierImpl();
+        metierCapt.setDao(new DaoImplCapt());
         System.out.println(metierCapt.calcul());
     }
 }
+
