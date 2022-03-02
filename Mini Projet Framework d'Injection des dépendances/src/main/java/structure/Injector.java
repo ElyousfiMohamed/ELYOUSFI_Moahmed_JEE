7074a920dcaf774deef977a134cee39d1843b659
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-03-02 17:35:18 +0100

    Framework (XML + Setter)

--- Mini Projet Framework d'Injection des dépendances/src/main/java/structure/Injector.java
+++ Mini Projet Framework d'Injection des dépendances/src/main/java/structure/Injector.java
@@ -9,14 +9,28 @@ import java.util.List;
 
 public class Injector {
 
+    private java.lang.Object object;
+
     public Injector(String filename) throws Exception{
         JAXBContext context = JAXBContext.newInstance(Objects.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
         Objects objs = (Objects) unmarshaller.unmarshal(new File(filename));
 
+        Object o = objs.getObjects().get(1);
+
         Class c1=Class.forName(objs.getObjects().get(0).getClassName());
-        Class c2=Class.forName(objs.getObjects().get(1).getClassName());
+        Class c2=Class.forName(o.getClassName());
 
+        String fctName = o.getProperty().getNom().substring(0,1).toUpperCase() + o.getProperty().getNom().substring(1);
 
+        java.lang.Object obj = c2.newInstance();
+        Method meth = obj.getClass().getMethod("set"+fctName,c1.getInterfaces()[0]);
+        meth.invoke(obj,c1.newInstance());
+
+        this.object = obj;
+    }
+
+    public java.lang.Object getObject() {
+        return object;
     }
 }
--- Mini Projet Framework d'Injection des dépendances/src/main/java/userSimulation/presentation.java
+++ Mini Projet Framework d'Injection des dépendances/src/main/java/userSimulation/presentation.java
@@ -1,11 +1,14 @@
 package userSimulation;
 
 import structure.*;
+import userSimulation.metier.IMetier;
 
 
 public class presentation {
     public static void main(String[] args) throws Exception {
         Injector context = new Injector("objects.xml");
+        IMetier metier= (IMetier) context.getObject();
+        System.out.println(metier.calcul());
     }
 }
 
