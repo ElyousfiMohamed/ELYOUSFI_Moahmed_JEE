ede6a021315fbea974db007a4268e1679cb311d5
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-03-02 18:59:36 +0100

    Framework (XML + Constructor)

--- Mini Projet Framework d'Injection des dépendances/src/main/java/structure/Injector.java
+++ Mini Projet Framework d'Injection des dépendances/src/main/java/structure/Injector.java
@@ -3,9 +3,12 @@ package structure;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Injector {
 
@@ -21,11 +24,15 @@ public class Injector {
         Class c1=Class.forName(objs.getObjects().get(0).getClassName());
         Class c2=Class.forName(o.getClassName());
 
-        String fctName = o.getProperty().getNom().substring(0,1).toUpperCase() + o.getProperty().getNom().substring(1);
+
+        /*String fctName = o.getProperty().getNom().substring(0,1).toUpperCase() + o.getProperty().getNom().substring(1);
 
         java.lang.Object obj = c2.newInstance();
-        Method meth = obj.getClass().getMethod("set"+fctName,c1.getInterfaces());
-        meth.invoke(obj,c1.newInstance());
+        Method meth = c2.getMethod("set"+fctName,c1.getInterfaces());
+        meth.invoke(obj,c1.newInstance());*/
+
+        Constructor constructor = c2.getConstructor(c1.getInterfaces());
+        java.lang.Object obj = constructor.newInstance(c1.newInstance());
 
         this.object = obj;
     }
--- Mini Projet Framework d'Injection des dépendances/src/main/java/userSimulation/metier/MetierImpl.java
+++ Mini Projet Framework d'Injection des dépendances/src/main/java/userSimulation/metier/MetierImpl.java
@@ -5,19 +5,20 @@ import userSimulation.dao.IDao;
 public class MetierImpl implements IMetier{
     private IDao dao;
 
-    /*public MetierImpl() {
-    }
-
     public MetierImpl(IDao dao) {
         this.dao = dao;
-    }*/
+    }
+
+    public MetierImpl() {
+    }
 
     @Override
     public double calcul() {
         return dao.getData()*5/Math.sin(15);
     }
 
-    public void setDao(IDao dao) {
+
+    /*public void setDao(IDao dao) {
         this.dao = dao;
-    }
+    }*/
 }
