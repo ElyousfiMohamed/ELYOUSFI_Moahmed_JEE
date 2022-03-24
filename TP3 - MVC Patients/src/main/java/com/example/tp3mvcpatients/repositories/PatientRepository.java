f22446d0d2774d28d1624c7bdb9ccf52c28cc62a
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-03-24 14:19:26 +0100

    TP3 - MVC | Modification

--- TP3 - MVC Patients/src/main/java/com/example/tp3mvcpatients/repositories/PatientRepository.java
+++ TP3 - MVC Patients/src/main/java/com/example/tp3mvcpatients/repositories/PatientRepository.java
@@ -4,8 +4,23 @@ import com.example.tp3mvcpatients.entities.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
     Page<Patient> findByNomContains(String keyword, Pageable pageable);
+    Patient findPatientById(Long id);
+
+    @Transactional
+    @Modifying
+    @Query("update Patient p set p.nom = :nom,p.dateNaissance = :ddn,p.malade = :malade,p.score = :score where p.id = :id")
+    void updatePatient(@Param(value = "id") long id,
+                       @Param(value = "nom") String nom,
+                       @Param(value = "ddn") String ddn,
+                       @Param(value = "malade") boolean malade,
+                       @Param(value = "score") int score);
 }
--- TP3 - MVC Patients/src/main/java/com/example/tp3mvcpatients/web/PatientController.java
+++ TP3 - MVC Patients/src/main/java/com/example/tp3mvcpatients/web/PatientController.java
@@ -5,35 +5,35 @@ import com.example.tp3mvcpatients.repositories.PatientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.List;
+import java.util.Optional;
 
 @Controller
-@AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
+
+    public PatientController(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
     @GetMapping(path = "/")
     public String home() {
         return "redirect:/index";
     }
 
     @GetMapping(path = "/index")
-    public String patients(Model model, @RequestParam(name = "page",defaultValue = "0") int page,
-                                        @RequestParam(name = "size",defaultValue = "5")int size,
-                                        @RequestParam(name = "keyword",defaultValue = "")String keyword) {
-        Page<Patient> patients = patientRepository.findByNomContains(keyword,PageRequest.of(page, size));
-        model.addAttribute("list",patients.getContent());
-        model.addAttribute("nbrPages",new int[patients.getTotalPages()]);
-        model.addAttribute("pagecourant",page);
-        model.addAttribute("keyword",keyword);
+    public String patients(Model model, @RequestParam(name = "page", defaultValue = "0") int page,
+                           @RequestParam(name = "size", defaultValue = "5") int size,
+                           @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+        Page<Patient> patients = patientRepository.findByNomContains(keyword, PageRequest.of(page, size));
+        model.addAttribute("list", patients.getContent());
+        model.addAttribute("nbrPages", new int[patients.getTotalPages()]);
+        model.addAttribute("pagecourant", page);
+        model.addAttribute("keyword", keyword);
         return "patients";
     }
 
@@ -44,12 +44,29 @@ public class PatientController {
     }
 
     @GetMapping(path = "/new")
-    public String nouveau(Model model){
+    public String nouveau(Model model) {
         return "ajout";
     }
 
     @GetMapping(path = "/create")
     public String nouveau(Model model
+            , @RequestParam(name = "nom", defaultValue = "") String nom,
+                          @RequestParam(name = "ddn", defaultValue = "") String ddn,
+                          @RequestParam(name = "malade", defaultValue = "") String malade,
+                          @RequestParam(name = "score", defaultValue = "0") int score) {
+        Patient p = new Patient();
+        p.setNom(nom);
+        p.setDateNaissance(ddn);
+        if (malade.equals("Non"))
+            p.setMalade(false);
+        else
+            p.setMalade(true);
+        p.setScore(score);
+        patientRepository.save(p);
+        return "redirect:/index";
+    }
+    @GetMapping(path = "/maj")
+    public String maj(Model model,@RequestParam(name = "id",defaultValue = "")Long id
             ,@RequestParam(name = "nom",defaultValue = "")String nom,
                           @RequestParam(name = "ddn",defaultValue = "") String ddn,
                           @RequestParam(name = "malade",defaultValue = "")String malade,
@@ -62,13 +79,18 @@ public class PatientController {
         else
             p.setMalade(true);
         p.setScore(score);
-        patientRepository.save(p);
+        patientRepository.updatePatient(id,p.getNom(),p.getDateNaissance(),p.isMalade(),p.getScore());
         return "redirect:/index";
     }
 
     @GetMapping(path = "/update")
-    public String modifier(Model model,Patient p) {
-        patientRepository.save(p);
+    public String modifier(Model model,Long id) {
+        Patient p = patientRepository.findPatientById(id);
+        model.addAttribute("id",p.getId());
+        model.addAttribute("nom",p.getNom());
+        model.addAttribute("date",p.getDateNaissance());
+        model.addAttribute("malade",p.isMalade());
+        model.addAttribute("score",p.getScore());
         return "modification";
     }
 }
