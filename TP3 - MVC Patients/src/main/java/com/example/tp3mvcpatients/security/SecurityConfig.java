4ab0963bbf3bbc4b6811140af361d8fd4dc15280
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-03-28 12:34:04 +0200

    TP3 - MVC Patients | jdbcAuthentication

--- TP3 - MVC Patients/src/main/java/com/example/tp3mvcpatients/security/SecurityConfig.java
+++ TP3 - MVC Patients/src/main/java/com/example/tp3mvcpatients/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.tp3mvcpatients.security;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,17 +11,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
+@Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        PasswordEncoder passwordEncoder = passwordEncoder();
+        DataSource dataSource = null;
+        /*PasswordEncoder passwordEncoder = passwordEncoder();
         auth.inMemoryAuthentication()
                 .withUser("user1").password(passwordEncoder.encode("1234")).roles("USER")
-                .and().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN");
+                .and().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN");*/
         // les utilisateurs qu'on le droit d'acceder a l'application seront stocker au mémoire
         // {noop} no password encoder
+
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("Select username as principal, password as credentials, active from users where username = ?")
+                .usersByUsernameQuery("select username as principal, role as role from users_roles where username = ?")
+                .rolePrefix("ROLE_")
+                .passwordEncoder(passwordEncoder);
 
     }
 
