67e89cedeac20e718c6dd4ed2fd4e3c299210384
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-04-01 23:06:11 +0200

    Readme update

--- TP3 - MVC Patients/src/main/java/com/example/tp3mvcpatients/security/SecurityConfig.java
+++ TP3 - MVC Patients/src/main/java/com/example/tp3mvcpatients/security/SecurityConfig.java
@@ -21,19 +21,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         DataSource dataSource = null;
-        /*PasswordEncoder passwordEncoder = passwordEncoder();
+        PasswordEncoder passwordEncoder = passwordEncoder();
         auth.inMemoryAuthentication()
                 .withUser("user1").password(passwordEncoder.encode("1234")).roles("USER")
-                .and().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN");*/
-        // les utilisateurs qu'on le droit d'acceder a l'application seront stocker au mémoire
+                .and().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN");
+
+        // les utilisateurs qu'on le droit d'acceder à l'application seront stocker au mémoire
         // {noop} no password encoder
 
-        PasswordEncoder passwordEncoder = passwordEncoder();
+        /*PasswordEncoder passwordEncoder = passwordEncoder();
         auth.jdbcAuthentication().dataSource(dataSource)
                 .usersByUsernameQuery("Select username as principal, password as credentials, active from users where username = ?")
                 .usersByUsernameQuery("select username as principal, role as role from users_roles where username = ?")
                 .rolePrefix("ROLE_")
-                .passwordEncoder(passwordEncoder);
+                .passwordEncoder(passwordEncoder);*/
 
     }
 
